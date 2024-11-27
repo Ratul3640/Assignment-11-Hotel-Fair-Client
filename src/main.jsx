658b2679202523cbda15b25ea1975e28344d9c37
Nahanur-islam <nahnanurism71@gmail.com>
2024-11-27 22:16:20 +0600

    rooms added

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 // import App from './App.jsx'
+import './index.css'
 import {
-  createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-]);
+import { router } from './Routes/Routes';
+import AuthProvider from './AuthProvider/AuthProvider';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+    <ToastContainer />
+  </React.StrictMode>,
 )
