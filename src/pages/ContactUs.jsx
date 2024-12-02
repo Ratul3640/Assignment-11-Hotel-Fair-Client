2fd6306d5be70f0ebfe29708e78f6377e45ce379
Nahanur-islam <nahnanurism71@gmail.com>
2024-12-02 11:10:26 +0600

    contact as completed

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -27,7 +27,7 @@ const ContactUs = () => {
                                         </svg>
 
                                         <span className="mx-2 text-gray-700 w-72 dark:text-gray-400">
-                                            Stadium Road, S S Road, Sirajganj Sadar, Sirajganj, Bangladesh
+                                           Raninagar,Noagaon,Rajshahi,Bangladesh
                                         </span>
                                     </p>
 
@@ -36,7 +36,7 @@ const ContactUs = () => {
                                             <path d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                                         </svg>
 
-                                        <span className="mx-2 text-gray-700 truncate w-72 dark:text-gray-400">(257) 563-7401</span>
+                                        <span className="mx-2 text-gray-700 truncate w-72 dark:text-gray-400">(257) 563-785</span>
                                     </p>
 
                                     <p className="flex items-start -mx-2">
